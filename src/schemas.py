c58506b904ebdbb7e95fc6e90194b5b28bea9a19
Maevyn <daniel.veenstra11@gmail.com>
2014-12-21 22:22:43 -0600

    Changes to schema.py, added buildings and plantations.

--- src/schemas.py
+++ src/schemas.py
@@ -1,6 +1,4 @@
-from pony.orm import *
 
-db = Database('sqlite', ':memory:')
 
 class Game(db.Entity):
 	gameID = PrimaryKey(int)
@@ -9,23 +7,40 @@ class Game(db.Entity):
 	EndTime = Required(str)
 
 class Turn(db.Entity):
-	gameID = set("Game")
+	gameID = set(Game)
 	EventNum = PrimaryKey(int)
 	RoundNum = Required(int)
 	PhaseNum = Required(int)
 	TurnNum = Required(int)
 	EventType = str
-	Governor = set("Player")
+	Governor = set(Player)
 
 class Player(db.Entity):
 	gameID = Required(Game)
 	playerID = PrimaryKey(int)
 	playerName = Required(str)
+	colonists = int
+	victoryPoints = int
+	Doubloons = int
 
 class Building(db.Entity):
 	gameID = Required(Game)
 	ownerID = Required(Player)
 	buildingID = PrimaryKey(int)
 	activated = bool
+
+class Plantation(db.Entity):
+	gameID = Required(Game)
+	ownerID = Required(Player)
+	plantationID = PrimaryKey(int)
+	activated = bool
+
+class Ships(db.Entity):
+	gameID = Required(Game)
+	shipID = Required(int)
+	Capacity = int
+	CropType = str
+	CropNum = int
 	
+
 
