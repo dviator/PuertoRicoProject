309613d88878ffc7c193fbf8db523bb0d0c09b89
Maevyn <daniel.veenstra11@gmail.com>
2014-12-21 21:09:58 -0600

    Properly moving file.

--- src/schemas.py
+++ src/schemas.py
@@ -1,3 +1,29 @@
 
 
-class 
+class Game(db.Entity):
+	gameID = PrimaryKey(int)
+	numOfPlayers = Required(int)
+	StartTime = Required(str)
+	EndTime = Required(str)
+
+class Turn(db.Entity):
+	gameID = set("Game")
+	EventNum = PrimaryKey(int)
+	RoundNum = Required(int)
+	PhaseNum = Required(int)
+	TurnNum = Required(int)
+	EventType = str
+	Governor = set("Player")
+
+class Player(db.Entity):
+	gameID = Required(Game)
+	playerID = PrimaryKey(int)
+	playerName = Required(str)
+
+class Building(db.Entity):
+	gameID = Required(Game)
+	ownerID = Required(Player)
+	buildingID = PrimaryKey(int)
+	activated = bool
+	
+
